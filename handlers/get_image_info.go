081f46d51f3c556917dfad9624fd27da5e79a5e1
evgensoft <evgensoft@yandex.ru>
2023-02-28 11:03:21 +0300

    Refactoring

--- handlers/get_image_info.go
+++ handlers/get_image_info.go
@@ -14,52 +14,14 @@ type Message struct {
 }
 
 func GetImageInfo(resp http.ResponseWriter, req *http.Request) {
-	if req.Body != nil {
-		defer req.Body.Close()
-	}
-
-	var reqMessage Message
-
-	err := json.NewDecoder(req.Body).Decode(&reqMessage)
-	if err != nil {
-		log.Error().Msgf("error decode json: %v", err)
-		http.Error(resp, err.Error(), http.StatusBadRequest)
-
-		return
-	}
-
-	log.Info().Msgf("Req %s %s - %+#v", req.Method, req.RequestURI, reqMessage)
-
-	if reqMessage.URL == "" {
-		http.Error(resp, "URL is null", http.StatusBadRequest)
-
-		return
-	}
-
-	ImageInfo(&reqMessage, false)
-
-	body, err := json.Marshal(&reqMessage)
-	if err != nil {
-		log.Error().Msgf("error json.Marshal: %v", err)
-		resp.WriteHeader(http.StatusInternalServerError)
-
-		return
-	}
-
-	resp.WriteHeader(http.StatusOK)
-	resp.Header().Set("Content-Type", "application/json")
-
-	log.Info().Msgf("Ans %s", string(body))
-
-	_, err = resp.Write(body)
-	if err != nil {
-		log.Error().Msgf("error write answer - %v", err)
-	}
-
-	debug.FreeOSMemory()
+	processRequest(resp, req, false)
 }
 
 func GetImageHash(resp http.ResponseWriter, req *http.Request) {
+	processRequest(resp, req, true)
+}
+
+func processRequest(resp http.ResponseWriter, req *http.Request, hash bool) {
 	if req.Body != nil {
 		defer req.Body.Close()
 	}
@@ -82,7 +44,7 @@ func GetImageHash(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	ImageInfo(&reqMessage, true)
+	ImageInfo(&reqMessage, hash)
 
 	body, err := json.Marshal(&reqMessage)
 	if err != nil {
